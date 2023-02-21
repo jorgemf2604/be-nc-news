2ebf0bc96b878b8b3833fe71de21c8c23678fb2a
Jorge Martin Fernandez <jorgemf2604@gmail.com>
2023-02-21 17:23:57 +0000

    clean commented code

--- models/comments-model.js
+++ models/comments-model.js
@@ -12,23 +12,6 @@ exports.fetchCommentsByArticleId = (id) => {
 };
 
 exports.insertCommentOnArticle = async (id, username, body) => {
-  // const articleIdIsValid = await checkArticleIdIsValid(id);
-  // const usernameIsValid = await checkUsernameIsValid(username);
-
-  // if (!usernameIsValid) {
-  //   return Promise.reject({
-  //     status: 404,
-  //     msg: `We could not find username`,
-  //   });
-  // }
-
-  // if (!articleIdIsValid) {
-  //   return Promise.reject({
-  //     status: 404,
-  //     msg: `We could not find the article id`,
-  //   });
-  // }
-
   if (body === undefined || username == undefined) {
     return Promise.reject({
       status: 400,
@@ -36,26 +19,10 @@ exports.insertCommentOnArticle = async (id, username, body) => {
     });
   }
 
-  // if
-
   const response = await db.query(
     "INSERT INTO comments (author, article_id, body) VALUES ($1, $2, $3) RETURNING *;",
     [username, id, body]
   );
 
   return response.rows[0];
-};
-
-const checkArticleIdIsValid = (articleId) => {
-  return db
-    .query("SELECT * FROM articles WHERE articles.article_id=$1", [articleId])
-    .then((response) => response.rows)
-    .then((articles) => !(articles.length === 0));
-};
-
-const checkUsernameIsValid = (username) => {
-  return db
-    .query("SELECT * FROM users WHERE username=$1;", [username])
-    .then((response) => response.rows)
-    .then((usernames) => !(usernames.length === 0));
 };
