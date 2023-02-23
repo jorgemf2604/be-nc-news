88359c544aba5217c8fd0abe8ca6141d344dd8f7
Jorge Martin Fernandez <jorgemf2604@gmail.com>
2023-02-23 17:05:41 +0000

    add routers

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
 const express = require("express");
-
 const { apiRouter } = require("./routes/api-router.js");
-
 const {
   handle500Errors,
   handleCustomErrors,
   handlePsqlErrors,
   handle404nonExistentPaths,
 } = require("./controllers/error-handling.js");
-const { getAllTopics } = require("./controllers/topics-controller.js");
-const {
-  getAllArticles,
-  getArticleById,
-  patchArticle,
-} = require("./controllers/articles-controller.js");
-const {
-  getCommentsByArticleId,
-  postCommentOnArticle,
-  deleteCommentById,
-} = require("./controllers/comments-controller.js");
-const { getAllUsers } = require("./controllers/users-controller.js");
-// const { getEndpoints } = require("./controllers/endpoints-controller");
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
-
-// app.get("/api", getEndpoints);
-app.get("/api/topics", getAllTopics);
-app.get("/api/articles", getAllArticles);
-app.get("/api/articles/:article_id", getArticleById);
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-app.post("/api/articles/:article_id/comments", postCommentOnArticle);
-app.patch("/api/articles/:article_id", patchArticle);
-app.get("/api/users", getAllUsers);
-app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.use(handle404nonExistentPaths);
 
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,7 +1,15 @@
 const apiRouter = require("express").Router();
+const articlesRouter = require("./articles-router.js");
+const topicsRouter = require("./topics-router.js");
+const commentsRouter = require("./comments-router.js");
+const usersRouter = require("./users-router.js");
 const { getEndpoints } = require("../controllers/endpoints-controller.js");
 
 apiRouter.get("/", getEndpoints);
+apiRouter.use("/articles", articlesRouter);
+apiRouter.use("/topics", topicsRouter);
+apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/users", usersRouter);
 
 module.exports = {
   apiRouter,
